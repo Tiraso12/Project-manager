cebe318f277d0114ac42ab9ffea2abd5607d7db9
Gonzalo Rodriguez <gonzaloostos02@gmail.com>
2024-09-05 22:22:32 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import NewProject from "./components/NewProject.jsx";
+import ProjectSidebar from "./components/ProjectsSidebar.jsx";
+import NoProjectSelected from "./components/NoProjectSelected.jsx";
+
+function App() {
+  return (
+    <main className=" h-screen my-8 flex gap-8">
+      <ProjectSidebar/>
+      {/* <NewProject/> */}
+      <NoProjectSelected/>
+    </main>
+  );
+}
+
+export default App;
