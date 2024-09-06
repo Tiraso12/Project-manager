73d8df244966535f38bd459c755784a39555096d
Gonzalo Rodriguez <gonzaloostos02@gmail.com>
2024-09-06 00:10:16 -0400

    added component and rendering it on click

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,40 @@
+// REACT UTILITIES
+import { useState } from "react";
+
+// COMPONENTS
 import NewProject from "./components/NewProject.jsx";
 import ProjectSidebar from "./components/ProjectsSidebar.jsx";
 import NoProjectSelected from "./components/NoProjectSelected.jsx";
 
 function App() {
+  const [projectsState, setProjectState] = useState({
+    selectedProjectId: undefined,
+    projects: []
+  });
+
+  const projectSelected = !!projectsState.selectedProject; // Check if a project is selected
+
+  function handleStartAddProject() {
+    setProjectState(prevState =>{
+      return{
+        selectedProjectId: null,
+      }
+    })
+  }
+
+  let content;
+
+  if(projectsState.selectedProjectId === null){
+    content = <NewProject/>
+  }else if (projectsState.selectedProjectId === undefined){
+    content = <NoProjectSelected/>
+  }
+
   return (
-    <main className=" h-screen my-8 flex gap-8">
-      <ProjectSidebar/>
-      {/* <NewProject/> */}
-      <NoProjectSelected/>
+    <main className="h-screen my-8 flex gap-8">
+      <ProjectSidebar onStartAddProject={handleStartAddProject} />
+      {/* <NewProject /> */}
+       {content} 
     </main>
   );
 }
