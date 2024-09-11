48955c5ef399f00e212f8b8c76b4a3c39eba0914
Gonzalo Rodriguez <gonzaloostos02@gmail.com>
2024-09-10 22:31:35 -0400

    new taks state added

--- src/components/NewTask.jsx
+++ src/components/NewTask.jsx
@@ -1,8 +1,23 @@
+import { useState } from "react";
+
 export default function NewTask(){
+    const [enteredTask, setEnteredTask] =useState(null);
+
+    function handleChange(event) {
+        setEnteredTask(event.target.value);
+        
+    }
+
+
     return(
         <div className="flex items-center gap-4"> 
-            <input type="text" className="w-64 px-2 py-1 rounded-sm bg-stone-200" />
-            <button className="textstone700 hover:text-stone-950">Add Task</button>
+            <input 
+            className="w-64 px-2 py-1 rounded-sm bg-stone-200"
+            type="text"
+            onChange={handleChange}
+            value={enteredTask}
+            />
+            <button  className="textstone700 hover:text-stone-950">Add Task</button>
         </div>
     )
 }
